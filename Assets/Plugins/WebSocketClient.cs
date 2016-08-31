2728aa6791ddd80f86fb50f6dd4580c126cfb000
MobileGameServer <mobilegameserver@gmail.com>
2016-08-31 23:32:15 +0900

    first commit

---
+++ Assets/Plugins/WebSocketClient.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using WebSocketSharp;
+
+public class WebSocketClient
+{
+	public bool isConnected = false;
+	public string error = null;
+
+	Uri uri;
+	WebSocket webSocket;
+	Queue<byte[]> messages = new Queue<byte[]>();
+
+	public WebSocketClient(Uri uri)
+	{
+		this.uri = uri;
+	}
+
+	public IEnumerator Connect()
+	{
+		webSocket = new WebSocketSharp.WebSocket(uri.ToString());
+		{
+			webSocket.OnOpen += (sender, e) => isConnected = true;
+			webSocket.OnMessage += (sender, e) => messages.Enqueue(e.RawData);
+			webSocket.OnError += (sender, e) => error = e.Message;
+			webSocket.OnClose += (sender, e) => isConnected = false;
+
+			webSocket.ConnectAsync();
+
+			while (!isConnected && error == null)
+			{
+				yield return 0;
+			}
+		}
+	}
+
+	public void Send(byte[] msg, Action<bool> onSendCompleted)
+	{
+		webSocket.SendAsync(msg, onSendCompleted);
+	}
+
+	public byte[] Receive()
+	{
+		if (messages.Count == 0)
+		{
+			return null;
+		}
+
+		return messages.Dequeue();
+	}
+
+	public void Close()
+	{
+		webSocket.CloseAsync();
+	}
+}
---
+++ Assets/Scripts/UnityWebSocketExample.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Text;
+
+class CreateUserReq
+{
+	public string userName;
+	public string passwd;
+}
+
+public class UnityWebSocketExample : MonoBehaviour {
+
+	void OnSendCompleted(bool result)
+	{
+		return;
+	}
+
+	// Use this for initialization
+	IEnumerator Start()
+	{
+		var ws = new WebSocketClient(new Uri("ws://echo.websocket.org"));
+		yield return StartCoroutine(ws.Connect());
+		
+		var req = new CreateUserReq();
+		{
+			req.userName = "userName";
+			req.passwd = "passwd";
+		}
+
+		var bytes = Encoding.UTF8.GetBytes(JsonUtility.ToJson(req));
+		ws.Send(bytes, OnSendCompleted);
+	}
+}
